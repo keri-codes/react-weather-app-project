a37a34db6cf254294cb902c1abf2114fc840478b
Keri Ritenour <keri@keriritenour.com>
2023-03-24 00:09:56 -0400

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by Keri Ritenour and is open-sourced on{" "}
-          <a
-            href="https://github.com/keri-codes/react-weather-app-project"
-            target="_blank"
-            rel="noreferrer"
-            alt="link to profile page on github"
-          >
-            Github
-          </a>
-          .
-        </p>
-      </footer>{" "}
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by Keri Ritenour and is open-sourced on{" "}
+            <a
+              href="https://github.com/keri-codes/react-weather-app-project"
+              target="_blank"
+              rel="noreferrer"
+              alt="link to profile page on github"
+            >
+              Github
+            </a>
+            .
+          </p>
+        </footer>{" "}
+      </div>
     </div>
   );
 }
