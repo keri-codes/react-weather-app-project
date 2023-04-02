1626fb1682e2533fb95eb6d57bc2f45254c8c102
Keri Ritenour <keri@keriritenour.com>
2023-04-02 01:16:27 -0400

    CSS Updates

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#1d355e"
       size={props.size}
       animate={true}
     />
