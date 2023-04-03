015545df41d2797e12c8b9c5f665dcdb60607590
Keri Ritenour <keri@keriritenour.com>
2023-04-02 23:05:24 -0400

    Styling and background image added

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1d355e"
+      color="#ffffff"
       size={props.size}
       animate={true}
     />
